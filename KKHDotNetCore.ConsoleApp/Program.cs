1e9589c39d790f2480460562527aa4673c256462
kyawlattninjavan <147364440+kyawlattninjavan@users.noreply.github.com>
2025-01-10 03:26:46 +0630

    feat: update sql connection source code for console app

--- KKHDotNetCore.ConsoleApp/Program.cs
+++ KKHDotNetCore.ConsoleApp/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
 Console.ReadKey();
+
+string connectionString = "Data Source=.;Initial Catalog=KKHDotNetCore;User ID=sa;Password=sa";
+Console.WriteLine("Connection String: " + connectionString);
+SqlConnection sqlConnection = new SqlConnection(connectionString);
+Console.WriteLine("Connection opening...");
+sqlConnection.Open();
+Console.WriteLine("Connection open...");
+
+string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog]";
+SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+Console.WriteLine("Connection closing...");
+sqlConnection.Close();
+Console.WriteLine("Connection close...");
+
+foreach(DataRow dr in  dt.Rows)
+{
+    Console.WriteLine(dr["BlogAuthor"]);
+}
+
+Console.ReadKey(true);
