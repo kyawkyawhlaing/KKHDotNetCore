e8ca49c0aba40faa48ad9eb50718fe14ca65d71c
kyawlattninjavan <147364440+kyawlattninjavan@users.noreply.github.com>
2025-01-26 23:47:07 +0630

    feat: web api with dapper

---
+++ KKHDotNetCore.RestApi/Controllers/BlogsDapperController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Dapper;
+using KKHDotNetCore.RestApi.Models.ViewModels;
+
+namespace KKHDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsDapperController : ControllerBase
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=KKHDotNetCore;User ID=sa;Password=sa";
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            List<BlogViewModel> lst;
+            using(IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "select * from tbl_blog where DeleteFlag=0;";
+                lst = db.Query<Models.ViewModels.BlogViewModel>(query).ToList();
+            }
+            return Ok(lst);  
+        }
+    }
+}
---
+++ KKHDotNetCore.RestApi/Models/ViewModels/BlogViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace KKHDotNetCore.RestApi.Models.ViewModels
+{
+    public class BlogViewModel
+    {
+        public int? BlogId { get; set; }
+
+        public string? BlogTitle { get; set; } = null!;
+
+        public string? BlogAuthor { get; set; } = null!;
+
+        public string? BlogContent { get; set; } = null!;
+
+        public bool DeleteFlag { get; set; }
+    }
+}
