3597773b33e9dade5a213ae1c4b1ee9a688b02c6
kyawlattninjavan <147364440+kyawlattninjavan@users.noreply.github.com>
2025-01-26 20:21:40 +0630

    feat: update code

--- KKHDotNetCore.ConsoleApp/EFcoreExample.cs
+++ KKHDotNetCore.ConsoleApp/EFcoreExample.cs
@@ -51,31 +51,45 @@ namespace KKHDotNetCore.ConsoleApp
             Console.WriteLine(item.DeleteFlag);
 
         }
-    }
 
-    public void Update(string title, string author, string content)
-    {
-        AppDbContext db = new AppDbContext();
-        var item = db.Blogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
-        if (item is null)
+        public void Update(string title, string author, string content)
         {
-            Console.WriteLine("Data not found");
-            return;
+            AppDbContext db = new AppDbContext();
+            var item = db.Blogs.AsNoTracking().FirstOrDefault(x => x.BlogTitle == title);
+            if (item is null)
+            {
+                Console.WriteLine("Data not found");
+                return;
+            }
+            if (string.IsNullOrEmpty(title))
+            {
+                item.BlogTitle = title;
+            }
+            if (string.IsNullOrEmpty(author))
+            {
+                item.BlogAuthor = author;
+            }
+            if (string.IsNullOrEmpty(content))
+            {
+                item.BlogContent = content;
+            }
+            db.Entry(item).State = EntityState.Modified;
+            var result = db.SaveChanges();
+            Console.WriteLine(result == 1 ? "Updating success" : "Updating failed");
         }
-        if (string.IsNullOrEmpty(title))
+
+        public void Delete(int id)
         {
-            item.BlogTitle = title;
+            AppDbContext db = new AppDbContext();
+            var item = db.Blogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                Console.WriteLine("Data not found");
+                return;
+            }
+            db.Entry(item).State = EntityState.Deleted;
+            var result = db.SaveChanges();
+            Console.WriteLine(result == 1 ? "Deleting success" : "Deleting failed");
         }
-        if (string.IsNullOrEmpty(author))
-        {
-            item.BlogAuthor = author;
-        }
-        if (string.IsNullOrEmpty(content))
-        {
-            item.BlogContent = content;
-        }
-        db.Entry(item).State = EntityState.Modified;
-        var result = db.SaveChanges();
-        Console.WriteLine(result == 1 ? "Updating success" : "Updating failed");
     }
 }
