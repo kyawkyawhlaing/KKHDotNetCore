c183a4927502fc821005c7f68b7154f37bf84ce4
kyawlattninjavan <147364440+kyawlattninjavan@users.noreply.github.com>
2025-01-26 20:01:53 +0630

    feat: add EF Core Update method

--- KKHDotNetCore.ConsoleApp/EFcoreExample.cs
+++ KKHDotNetCore.ConsoleApp/EFcoreExample.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,5 +34,48 @@ namespace KKHDotNetCore.ConsoleApp
             int result = db.SaveChanges();
             Console.WriteLine(result == 1 ? "Adding success" : "Adding failed");
         }
+
+        public void Edit(int id )
+        {
+            AppDbContext db = new AppDbContext();
+            var item = db.Blogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if ( item is null )
+            {
+                Console.WriteLine("Data not found");
+                return;
+            }
+            Console.WriteLine(item.BlogId);
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+            Console.WriteLine(item.DeleteFlag);
+
+        }
+    }
+
+    public void Update(string title, string author, string content)
+    {
+        AppDbContext db = new AppDbContext();
+        var item = db.Blogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+        if (item is null)
+        {
+            Console.WriteLine("Data not found");
+            return;
+        }
+        if (string.IsNullOrEmpty(title))
+        {
+            item.BlogTitle = title;
+        }
+        if (string.IsNullOrEmpty(author))
+        {
+            item.BlogAuthor = author;
+        }
+        if (string.IsNullOrEmpty(content))
+        {
+            item.BlogContent = content;
+        }
+        db.Entry(item).State = EntityState.Modified;
+        var result = db.SaveChanges();
+        Console.WriteLine(result == 1 ? "Updating success" : "Updating failed");
     }
 }
