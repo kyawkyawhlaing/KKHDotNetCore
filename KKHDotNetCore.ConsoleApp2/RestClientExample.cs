51052e0532b6a8de261ace4000533dd6219ca0a6
kyawlattninjavan <147364440+kyawlattninjavan@users.noreply.github.com>
2025-02-02 14:44:29 +0630

    Add: RestClient Example

---
+++ KKHDotNetCore.ConsoleApp2/RestClientExample.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKHDotNetCore.ConsoleApp2
+{
+    public class RestClientExample
+    {
+        private readonly RestClient _client;
+        private readonly string _postEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+        public RestClientExample(RestClient client)
+        {
+            _client = client;
+        }
+
+        public async Task Read()
+        {
+            var request = new RestRequest(_postEndpoint, Method.Get);
+            var response = await _client.ExecuteAsync(request);
+            if(response.IsSuccessStatusCode)
+            {
+                string jsonString = response.Content!;
+                Console.WriteLine(jsonString);
+            }
+        }
+    }
+}
